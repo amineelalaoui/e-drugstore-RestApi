69041a3ec84ce764bdd7bb639705e56af61f7ed0
amineelalaoui <amine.elalaoui.med@gmail.com>
2020-05-22 05:46:59 +0200

    implement addCatalog @CatalogController

--- src/Controller/CatalogController.java
+++ src/Controller/CatalogController.java
@@ -1,13 +1,31 @@
 package controller;
 
+import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import model.Catalog;
+import service.CatalogService;
 
 @Path("catalog")
 @RequestScoped
 public class CatalogController {
-
 	
+	@EJB
+	private CatalogService catalogService;
+
+	@POST
+	@Path("/add")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addCatalog(Catalog catalog) {
+		return catalogService.addCatalog(catalog);
+	}
 	
 	
 }
